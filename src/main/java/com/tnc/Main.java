024779fbd2280ad4b02a29bfa49f33f0d7d11610
bogdan tancau <bogdan.tancau@atos.net>
2023-03-27 12:42:20 +0300

    add diference to check

--- src/main/java/com/tnc/Main.java
+++ src/main/java/com/tnc/Main.java
@@ -5,19 +5,20 @@ import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
-        String readFile = "Emails_updates_23_03_2023";
-        ArrayList<String> readArray = new ArrayList<>();
-        ArrayList<String> writeArray = new ArrayList<>();
-        ArrayList<String> difArray = new ArrayList<>();
+    static String readFile = "Emails_updates_23_03_2023 - Copy";
+    //        ArrayList<String> difArray = new ArrayList<>();
 //        File fileForRead = new File("C:/Users/a830083/Desktop/Migration_Project/email-Gabi/" + readFile + ".txt");
 //        File fileForWrite = new File("C:/Users/a830083/Desktop/Migration_Project/email-Gabi/CheckFile.txt");
-        File fileForRead = new File("C:/Users/a830083/Desktop/Migration_Project/email-Gabi/" + readFile + ".txt");
-        File fileForWrite = new File("C:/Users/a830083/Desktop/Migration_Project/email-Gabi/test.txt");
-        File difFile = new File("C:/Users/a830083/Desktop/Migration_Project/email-Gabi/difFile.txt");
+    static File fileForRead = new File("C:/Users/a830083/Desktop/Migration_Project/email-Gabi/" + readFile + ".txt");
+    static File fileForWrite = new File("C:/Users/a830083/Desktop/Migration_Project/email-Gabi/test.txt");
+    static File difFile = new File("C:/Users/a830083/Desktop/Migration_Project/email-Gabi/difFile.txt");
+
+    public static void main(String[] args) throws IOException {
+        ArrayList<String> readArray = new ArrayList<>();
+        ArrayList<String> writeArray = new ArrayList<>();
         readFromTheFile(readArray, fileForRead);
         writeToTheFile(writeArray, fileForWrite);
-        appendToTheBaseFile(readArray, writeArray, difArray, fileForWrite);
+        appendToTheBaseFile(readArray, writeArray, fileForWrite, difFile);
     }
 
     private static void readFromTheFile(ArrayList<String> readArray, File fileForRead) {
@@ -38,20 +39,25 @@ public class Main {
         }
     }
 
-    private static void appendToTheBaseFile(ArrayList<String> rows, ArrayList<String> writeArray, ArrayList<String> difArray, File file2) throws IOException {
+    private static void appendToTheBaseFile(ArrayList<String> rows, ArrayList<String> writeArray, File file2, File difFile) throws IOException {
+        ArrayList<String> difArray = new ArrayList<>();
+        FileOutputStream writeToBaseFile = new FileOutputStream(file2, true);
+        FileOutputStream writeToDifFile = new FileOutputStream(difFile, true);
         for (String word : rows) {
-            FileOutputStream writeToBaseFile = new FileOutputStream(file2, true);
+            StringBuilder addString = new StringBuilder();
             var charArr = word.toCharArray();
-            String addString = "";
             for (char c : charArr) {
                 if (!Character.isDigit(c)) {
-                    addString += (String.valueOf(c));
+                    addString.append(c);
                 }
             }
-            if (!writeArray.contains(word) && !writeArray.contains(addString)) {
+            if (!writeArray.contains(word) && !writeArray.contains(addString.toString())) {
                 writeArray.add("\n" + addString);
                 difArray.add("\n" + addString);
                 writeToBaseFile.write(("\n" + addString).getBytes());
+                if (!difArray.contains(word)) {
+                    writeToDifFile.write(("\n" + readFile.substring(15, 23) + " *** diff to check ***" + "\n " + addString).getBytes());
+                }
             }
         }
         System.out.println(difArray);
